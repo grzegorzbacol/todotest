c81fce72431905fa3ef0b7aa18d0d8569119dd1a
Grzegorz Bącol <grzegorzbacol@MacBook-Pro-Grzegorz.local>
2026-01-05 21:05:56 +0100

    Fix: Remove @routes directive that requires Ziggy package

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'Todo App') }}</title>
-        @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx'])
         @inertiaHead
