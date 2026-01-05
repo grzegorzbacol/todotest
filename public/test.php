a481594c4e7e841cee8d7555352594fcc81ab37f
Grzegorz Bącol <grzegorzbacol@MacBook-Pro-Grzegorz.local>
2026-01-05 21:27:10 +0100

    Add simple PHP test file

---
+++ public/test.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+echo "PHP is working!\n";
+echo "PHP Version: " . phpversion() . "\n";
+echo "Current time: " . date('Y-m-d H:i:s') . "\n";
+
