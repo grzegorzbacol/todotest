2a65618d76b613331b3bb0c2c5d1bfe4110a98cb
Grzegorz Bącol <grzegorzbacol@MacBook-Pro-Grzegorz.local>
2026-01-06 11:51:07 +0100

    Add TrustedProxies configuration for Caddy reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
+        
+        // Trust all proxies (Caddy, Traefik, etc.)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
